4198878a51bba8e799a4a5a8cd3bd7192ddb02fa
atsuky <andreugallofre@gmail.com>
2019-04-06 16:45:05 +0200

    Add first version of code for the gyroscope sensor

---
+++ hardware/arduino/sensor.ino
@@ -0,0 +1,112 @@
+#include "CurieIMU.h"
+int ax, ay, az;         // accelerometer values
+int gx, gy, gz;         // gyrometer values
+
+const int ledPin = 13;      // activity LED pin
+boolean blinkState = false; // state of the LED
+
+int calibrateOffsets = 1; // int to determine whether calibration takes place or not
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  while (!Serial);    // wait for the serial port to open
+
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  if (CurieIMU.begin()) {
+    Serial.println("CurieIMU connection successful");
+  } else {
+    Serial.println("CurieIMU connection failed");
+  }
+
+  // use the code below to calibrate accel/gyro offset values
+  if (calibrateOffsets == 1) {
+    Serial.println("Internal sensor offsets BEFORE calibration...");
+    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
+    Serial.print("\t"); // -76
+    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
+    Serial.print("\t"); // -235
+    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
+    Serial.print("\t"); // 168
+    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
+    Serial.print("\t"); // 0
+    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
+    Serial.print("\t"); // 0
+    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
+
+    // To manually configure offset compensation values,
+    // use the following methods instead of the autoCalibrate...() methods below
+    //CurieIMU.setAccelerometerOffset(X_AXIS,495.3);
+    //CurieIMU.setAccelerometerOffset(Y_AXIS,-15.6);
+    //CurieIMU.setAccelerometerOffset(Z_AXIS,491.4);
+    //CurieIMU.setGyroOffset(X_AXIS,7.869);
+    //CurieIMU.setGyroOffset(Y_AXIS,-0.061);
+    //CurieIMU.setGyroOffset(Z_AXIS,15.494);
+
+    Serial.println("About to calibrate. Make sure your board is stable and upright");
+    delay(5000);
+
+    // The board must be resting in a horizontal position for
+    // the following calibration procedure to work correctly!
+    Serial.print("Starting Gyroscope calibration and enabling offset compensation...");
+    CurieIMU.autoCalibrateGyroOffset();
+    Serial.println(" Done");
+
+    Serial.print("Starting Acceleration calibration and enabling offset compensation...");
+    CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
+    CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
+    CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
+    Serial.println(" Done");
+
+    Serial.println("Internal sensor offsets AFTER calibration...");
+    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
+    Serial.print("\t"); // -76
+    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
+    Serial.print("\t"); // -2359
+    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
+    Serial.print("\t"); // 1688
+    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
+    Serial.print("\t"); // 0
+    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
+    Serial.print("\t"); // 0
+    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
+  }
+  
+  // configure Arduino LED for activity indicator
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read raw accel/gyro measurements from device
+  CurieIMU.readMotionSensor(ax, ay, az, gx, gy, gz);
+
+  // these methods (and a few others) are also available
+
+  //CurieIMU.readAcceleration(ax, ay, az);
+  //(gx, gy, gz);
+
+  ax = CurieIMU.readAccelerometer(X_AXIS);
+  ay = CurieIMU.readAccelerometer(Y_AXIS);
+  az = CurieIMU.readAccelerometer(Z_AXIS);
+  gx = CurieIMU.readGyro(X_AXIS);
+  gy = CurieIMU.readGyro(Y_AXIS);
+  gz = CurieIMU.readGyro(Z_AXIS);
+
+  // display tab-separated accel/gyro x/y/z values
+  Serial.print("a/g:\t");
+  Serial.print(ax);
+  Serial.print("\t");
+  Serial.print(ay);
+  Serial.print("\t");
+  Serial.print(az);
+  Serial.print("\t");
+  Serial.print(gx);
+  Serial.print("\t");
+  Serial.print(gy);
+  Serial.print("\t");
+  Serial.println(gz);
+}
