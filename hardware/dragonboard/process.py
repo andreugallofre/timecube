bdf4e495d3d155454d6619d36d6bca35cd598b48
atsuky <andreugallofre@gmail.com>
2019-04-07 04:52:09 +0200

    process modified

--- hardware/dragonboard/process.py
+++ hardware/dragonboard/process.py
@@ -1,25 +1,37 @@
 import json
 import time
+import requests
+
+API_ENDPOINT = "http://10.105.112.73:8080/api/cube/canvi"
 
 def process (data, cara):
     if data["accel_z"] < -14000:
-        return 3
-    elif data["accel_y"] < -14000:
-        return 4
-    elif data["accel_x"] < -14000:
-        return 5
-    elif data["accel_z"] > 14000:
-        return 1
-    elif data["accel_y"] > 14000:
         return 2
+    elif data["accel_y"] < -14000:
+        return 3
+    elif data["accel_x"] < -14000:
+        return 4
+    elif data["accel_z"] > 14000:
+        return 0
+    elif data["accel_y"] > 14000:
+        return 1
     elif data["accel_x"] > 14000:
-        return 6
+        return 5
     
     return cara
 
-old_cara = 1
-cara = 1
-new_cara = 1
+def send_request (old, new):
+    if old == -1:
+        r = requests.post(url = API_ENDPOINT, json={"code": 1234, "actual": new})
+    else:
+        r = requests.post(url = API_ENDPOINT, json={"code": 1234, "anterior": old, "actual": new})
+    print(r)
+    return r
+
+old_cara = 0
+cara = 0
+new_cara = 0
+first = True
 
 while True:
     time.sleep(1)
@@ -36,3 +48,8 @@ while True:
         cara = new_cara
         print("Cara actual: " + str(cara) + " Cara antiga: " + str(old_cara))
         #Send request
+        if first:
+            send_request( -1, cara)
+            first = False
+        else:
+            send_request(old_cara, cara)
